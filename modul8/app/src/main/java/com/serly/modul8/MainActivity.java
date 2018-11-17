377c8c4aee512102ab467bcf73dcfc68c977ccb4
serlynovita <serlyvita98@gmail.com>
2018-11-17 21:53:43 +0700

    tugas modul 8
    tugas modul 8

---
+++ modul8/app/src/main/java/com/serly/modul8/MainActivity.java
@@ -0,0 +1,131 @@
+package com.serly.modul8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.serly.modul8.Adapter.MyAdapter;
+import com.serly.modul8.Model.GetPembelian;
+import com.serly.modul8.Model.Pembelian;
+//import com.serly.modul8.Model.PostPutDelPembelian;
+import com.serly.modul8.Model.PostPutDelPembelian;
+import com.serly.modul8.Rest.ApiClient;
+import com.serly.modul8.Rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+//    Button btGet, btUpdate, btInsert, btDelete;
+    Button btGet;
+    ApiInterface mApiInterface;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btGet = (Button) findViewById(R.id.btGet);
+//        btUpdate = (Button) findViewById(R.id.btUpdate);
+//        btInsert = (Button) findViewById(R.id.btInsert);
+//        btDelete = (Button) findViewById(R.id.btDelete);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mApiInterface  = ApiClient.getClient().create(ApiInterface.class);
+
+        btGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<GetPembelian> pembelianCall = mApiInterface.getPembelian();
+                pembelianCall.enqueue(new Callback<GetPembelian>() {
+                    @Override
+                    public void onResponse(Call<GetPembelian> call, Response<GetPembelian> response) {
+                        List<Pembelian> pembelianList = response.body().getListDataPembelian();
+                        Log.d("Retrofit Get", "Jumlah data pembelian: " + String.valueOf(pembelianList.size()));
+
+                        mAdapter = new MyAdapter(pembelianList);
+                        mRecyclerView.setAdapter(mAdapter);
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<GetPembelian> call, Throwable t) {
+                        // Log error
+                        Log.e("Retrofit Get", t.toString());
+                    }
+                });
+            }
+        });
+
+//        btUpdate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Call<PostPutDelPembelian> updatePembelianCall = mApiInterface.putPembelian("beli16","pemb11","2016-10-23","500000","tiket11");
+//                updatePembelianCall.enqueue(new Callback<PostPutDelPembelian>() {
+//                    @Override
+//                    public void onResponse(Call<PostPutDelPembelian> call, Response<PostPutDelPembelian> response) {
+//                        Log.d("Retrofit Update", "Status Update: " + String.valueOf(response.body().getStatus()));
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<PostPutDelPembelian> call, Throwable t) {
+//                        Log.d("Retrofit Update", t.getMessage());
+//                    }
+//                });
+//            }
+//        });
+//
+//        btInsert.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Call<PostPutDelPembelian> postPembelianCall = mApiInterface.postPembelian("16",
+//                        "pemb11","2016-10-23","500000","tiket11");
+//                postPembelianCall.enqueue(new Callback<PostPutDelPembelian>() {
+//                    @Override
+//                    public void onResponse(Call<PostPutDelPembelian> call, Response<PostPutDelPembelian> response) {
+//                        Log.d("Retrofit Insert", "Status Insert: " + String.valueOf(response.body().getStatus()));
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<PostPutDelPembelian> call, Throwable t) {
+//                        Log.d("Retrofit Insert", t.getMessage());
+//                    }
+//                });
+//            }
+//        });
+//
+//        btDelete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Call<PostPutDelPembelian> deletePembelian = mApiInterface.deletePembelian("16");
+//                deletePembelian.enqueue(new Callback<PostPutDelPembelian>() {
+//                    @Override
+//                    public void onResponse(Call<PostPutDelPembelian> call, Response<PostPutDelPembelian> response) {
+//                        Log.i("Retrofit Delete", "Status Delete: " + String.valueOf(response.body().getStatus()));
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<PostPutDelPembelian> call, Throwable t) {
+//                        Log.i("Retrofit Delete", t.getMessage());
+//                    }
+//                });
+//            }
+//        });
+//
+//
+    }
+ }
---
+++ modul8/app/src/main/java/com/serly/modul8/Model/PostPutDelPembelian.java
@@ -0,0 +1,37 @@
+package com.serly.modul8.Model;
+import com.google.gson.annotations.SerializedName;
+
+public class PostPutDelPembelian {
+    @SerializedName("status")
+    String status;
+    @SerializedName("result")
+    Pembelian mPembelian;
+    @SerializedName("message")
+    String message;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Pembelian getPembelian() {
+        return mPembelian;
+    }
+
+    public void setPembelian(Pembelian pembelian) {
+        mPembelian = pembelian;
+    }
+
+}
